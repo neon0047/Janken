ceb51072bd3cc6c7ef3d3b6847c5341341b0749a
Nagendra Singh <56041956+neon0047@users.noreply.github.com>
2020-06-11 02:18:45 +0530

    Add files via upload

---
+++ login.php
@@ -0,0 +1,57 @@
+<?php
+
+if (isset($_POST["cancel"])){
+	header("Location: index.php");
+	return;
+}
+
+$salt = 'XyZzy12*_';
+$stored_hash = '1a52e17fa899cf40fb04cfc42e6352f1';
+
+$failure = false;
+
+if (isset($_POST['who']) && isset($_POST['pass'])){
+	if (strlen($_POST['who'])<1 || strlen($_POST['pass'])<1){
+       $failure = 'User name and password are required';
+	
+       
+    }else{
+    	$check = hash('md5', $salt.$_POST['pass']);
+    	if ($check == $stored_hash){
+    	header("Location: game.php?name=".urlencode($_POST['who']));
+        }else{
+    	$failure = "Wrong Password";
+        } 
+    }
+}
+?>
+
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Nagendra Pal Sing - Rock Paper Scissors</title>
+	<link rel = "stylesheet" href = "style.css">
+	<?php require_once "bootstrap.php"; ?>
+</head>
+<body>
+	<div class = "container">
+		<h1>Please Log In</h1>
+<?php
+
+if ($failure !== false){
+	echo('<p style = "color:red;">'.htmlentities($failure)."</p>\n");
+}
+?>
+
+		<form method="POST">
+		<label for= "nam">User Name :</label>
+			<input type= "text" name ="who" id = "nam" ></br>
+		<label for = "id_1723" >Password:</label>
+		    <input type = "text" name = "pass" id = "id_1723"></br>
+		    <input type = "submit" value = "Log In">
+		    <input type = "submit" value = "cancel" name ="cancel">
+		</form>    	
+</div>
+</body>
+</html>
