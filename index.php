db574aa7edcb3265c5ac19f2af391706fd014901
Nagendra Singh <56041956+neon0047@users.noreply.github.com>
2020-06-11 02:18:05 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Nagendra Pal Sing - Rock Paper Scissors</title>
+	<?php require_once "bootstrap.php"; ?>	<link rel = "stylesheet" href = "style.css">
+</head>
+<body>
+	<div class = "container">
+		<h1>Welcome to Rock Paper Scissors</h1>
+		<p><a href = "login.php">Please Log In</a></p>
+		<p>Attemp to go to the <a href = "game.php">game.php</a> without logging in - it should fail with an error message.</p>
+</div>
+</body>
+</html>
